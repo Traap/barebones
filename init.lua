eeb9b0e4624847047b405553bca05c9c9b36ffdf
Traap <gary.a.howard@mac.com>
2024-08-04 14:44:16 -0700

    chore: update init.lua

--- init.lua
+++ init.lua
@@ -1,21 +1,42 @@
+-- {{{ Bootstraap lazy.nvim when needed.
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
--- Bootstrap lazy.nvim
-if not vim.loop.fs_stat(lazypath) then
-  -- stylua: ignore
-  vim.fn.system({
+local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local out = vim.fn.system({
     "git",
     "clone",
     "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", lazypath
+    "--branch=stable",
+    lazyrepo,
+    lazypath,
   })
-end
-vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+-- ------------------------------------------------------------------------- }}}
+-- {{{ Setup lazy.nvim
+--     https://lazy.folke.io/spec
 require("lazy").setup({
 	-- Setup lazy
 	spec = {
-    { import = "plugins"}
+		{ "LazyVim/LazyVim",
+      import = "lazyvim.plugins",
+    },
+    { import = "plugins" }
 	},
 
 	defaults = { lazy = false, version = false },
@@ -28,8 +49,6 @@ require("lazy").setup({
 		rtp = {
 			disabled_plugins = {
 				"gzip",
-        "matchit",
-        "matchparen",
 				"netrwPlugin",
 				"tarPlugin",
 				"tohtml",
@@ -39,3 +58,5 @@ require("lazy").setup({
 		},
 	},
 })
+
+-- ------------------------------------------------------------------------- }}}
